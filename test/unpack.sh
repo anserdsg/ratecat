4e726bda6e8536c3d23cf10701e155a2bfadbd22
Arlo Liu <arlo.liu@gmail.com>
2025-02-08 18:16:28 +0800

    update

---
+++ test/unpack.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# check arguments
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <input_file> <output_file> <password>"
+    exit 1
+fi
+
+input_file=$1
+output_file=$2
+password=$3
+
+# check if input_file exists
+if [ ! -f $input_file ]; then
+    echo "Error: $input_file does not exist"
+    exit 1
+fi
+
+temp_file=$(mktemp)
+base64 -d $input_file > $temp_file
+
+# decrypt the input file by openssl with pbkdf2 key derivation
+openssl enc -d -aes-256-cbc -salt -pbkdf2 -in $temp_file -out $output_file -k $password
+
+rm -f $temp_file
+
+echo "File $input_file is decrypted to $output_file"
